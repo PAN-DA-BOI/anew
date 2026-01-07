dbae0c456600766fb593e0fec8b38e6e05fdfd4f
Sable Evans <brodyevans82@gmail.com>
2026-01-07 13:25:31 -0700

    Update headgear.sh

--- headgear/headgear.sh
+++ headgear/headgear.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 echo "Updating and upgrading system..."
-apt-get update && apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 
-mv ./anew/headgear/* /root/files
+mv ./anew/headgear/software/* /root/files
 rm -rf ./anew/
-cd /root/files
+cd /root/files/
+
+#install all the nessisary packages
+sudo apt install libopencv-dev python3-opencv
+sudo apt install libspi-dev
+sudo apt install build-essential g++
+  sudo usermod -aG video,spi $USER
+
+g++ -o main /root/files/main.cpp `pkg-config --cflags --libs opencv4`
+
 
 #Terran, this is where the window manager needs to be installed and set up so that "/root/files/software/main" is the only thing that boots up onscreen
 apt-get install i3
@@ -20,7 +29,7 @@ apt-get install i3
 
 #REUSABLE SERVICE MAKER SO I DONT HAVE TO FIND THAT FORUM EVERYTIME
 SERVICE_NAME="control"
-EXECUTABLE_PATH="/headgear/software/pi/support"  # replace with the actual path
+EXECUTABLE_PATH="/root/files/support"  # replace with the actual path
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
 echo "[Unit]
@@ -48,7 +57,7 @@ sudo systemctl status "$SERVICE_NAME" --no-pager
 #################################################################################################
 #REUSABLE SERVICE MAKER SO I DONT HAVE TO FIND THAT FORUM EVERYTIME
 SERVICE_NAME="eyes"
-EXECUTABLE_PATH="/headgear/software/pi/main"  # replace with the actual path
+EXECUTABLE_PATH="/root/files/main"  # replace with the actual path
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
 echo "[Unit]
@@ -73,6 +82,5 @@ echo "Service status:"
 sudo systemctl status "$SERVICE_NAME" --no-pager
 #END OF REUSABLE SERVICE SCRIPT
 
-
-
-
+echo "Rebooting to apply changes..."
+sudo reboot
