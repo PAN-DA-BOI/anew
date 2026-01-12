191b9c2195d2de6db1f541150ecac5d9a8705573
Sable Evans <brodyevans82@gmail.com>
2026-01-12 14:58:05 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,19 @@
-#!/bin/sh
-sudo apt install openbox
+#!/bin/bash
+
+headear_flag = "false"
+arm_flag = "false"
+sensekit_flag = "false"
+tamale_flag= "false"
+
+while getopts ':ahst:' flag; do
+  case "${flag}" in
+    a) arm_flag="true" ;;
+    h) headear_flag="true" ;;
+	s) sensekit_flag="true" ;;
+	t) tamale_flag="true" ;;
+  esac
+done 
+
+if [  ]; then # for each optoin, chnage what gets moved or deleted
+    echo "Remaining positional arguments: $@"
+fi
